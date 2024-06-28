fe454f2be4cd8ee3b184a25185d3da94191643d3
JuniorDCoder <foryoungjuniorngu@gmail.com>
2024-06-28 19:46:53 +0100

    feat: Added app for pages

--- pages/views.py
+++ pages/views.py
@@ -2,5 +2,11 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def homePageView(request):
-    return HttpResponse('Hello, World!')
+from django.views.generic import TemplateView
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
